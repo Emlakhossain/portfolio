6300c56a8938795df42a2a098867f33918cb3d6e
Emlak-hossain <imranmarin1990@gmail.com>
2022-12-26 23:54:09 +0600

    commit on all updated

--- src/Page/Project/Project.js
+++ src/Page/Project/Project.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const Project = ({ project }) => {
-    const { image, projectName } = project;
+    const { image, projectName, url } = project;
     return (
         <div class="card w-68  ring-2 border-green-400 hover:cursor-pointer">
             <figure><img src={image} alt="Shoes" className='p-5 ' /></figure>
             <div class="card-body text-gray">
                 <h2 class="card-title">
                     {projectName}
-                    <div class="badge badge-secondary">NEW</div>
                 </h2>
+                <div className='flex justify-center'>
+                    <button className=' btn btn-info mt-4 font-bold text-white w-3/4'><a href={url} target_blank>More Info</a></button>
+                </div>
+
             </div>
         </div>
     );
