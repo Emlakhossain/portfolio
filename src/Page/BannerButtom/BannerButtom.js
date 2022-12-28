b90a46d384a4e72b2c4cbc5bfd2c197b494a34dc
Emlak-hossain <imranmarin1990@gmail.com>
2022-12-29 00:50:49 +0600

    added

--- src/Page/BannerButtom/BannerButtom.js
+++ src/Page/BannerButtom/BannerButtom.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import { FaAddressCard, FaHeadset, FaRegBookmark } from 'react-icons/fa';
+import { IconName, VscTools } from "react-icons/vsc";
 
 const BannerButtom = () => {
     return (
         <div className='flex justify-around items-center mx-auto  bg-white drop-shadow-2xl w-3/4 text-center -mt-10 h-32 rounded-full'>
-            <div>
-                <h4 className='font-bold'>2 Years Job</h4>
-                <p className='text-info'>Experience</p>
+            <div className='flex justify-center items-center'>
+                <div className='text-info text-4xl'>
+                    <FaRegBookmark />
+                </div>
+
+                <div className='px-4'>
+                    <h4 className='font-bold'>2 Years Job</h4>
+                    <p className='text-info'>Experience</p>
+                </div>
             </div>
-            <div>
-                <h4 className='font-bold'>50+ Projects</h4>
-                <p className='text-info'>Completed</p>
+            <div className='flex justify-center items-center'>
+                <div className='text-info text-4xl'>
+                    <FaAddressCard />
+                </div>
+                <div className='px-4'>
+                    <h4 className='font-bold'>50+ Projects</h4>
+                    <p className='text-info'>Completed</p>
+                </div>
             </div>
-            <div>
-                <h4 className='font-bold'>Support 24/7</h4>
-                <p className='text-info'>Online</p>
+            <div className='flex justify-center items-center'>
+                <div className='text-info text-4xl'>
+                    <FaHeadset />
+                </div>
+                <div className='px-4'>
+                    <h4 className='font-bold'>Support 24/7</h4>
+                    <p className='text-info'>Online</p>
+                </div>
             </div>
         </div>
     );
--- src/Page/Features/Feature.js
+++ src/Page/Features/Feature.js
@@ -3,13 +3,15 @@ import './Feature.css';
 import { FaDesktop } from "react-icons/fa";
 
 const Feature = ({ feature }) => {
-    const { featuresName, discription } = feature;
+
+    const { featuresName, discription, icons } = feature;
+
+
     return (
         <div class="card w-96 pt-10 content-center h-72 shadow-lg border rounded-tl-lg ...">
             <div class="card-body text-start ">
 
                 <p className='-mt-24 '><FaDesktop className='icon p-2' /></p>
-
                 <h2 class="card-title font-bold text-stone-900 ">{featuresName}</h2>
                 <p>{discription}</p>
 
