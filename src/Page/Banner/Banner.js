4534ddb238f8f15223f145211b7473f7d6f80ec3
Emlak-hossain <imranmarin1990@gmail.com>
2023-01-01 22:22:37 +0600

    added another commit

--- src/Page/Banner/Banner.js
+++ src/Page/Banner/Banner.js
@@ -16,7 +16,7 @@ const Banner = () => {
         <div className='grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 py-20 bg-gray-200 '>
             <div className='height-300 p-5'>
 
-                <p className='text-start px-5 font-bold text-info text-2xl'>Hi, I,m</p>
+                <p className='text-start px-5 font-bold text-info text-2xl'>Hi, I'm</p>
                 <h1 className='text-start px-5 font-bold text-5xl'> EMLAK HOSSAIN</h1>
                 <p className='text-start p-5'>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
                 <div className='flex gap-10'>
@@ -33,7 +33,7 @@ const Banner = () => {
                 </div>
             </div>
             <div className='pl-20 rounded-2'>
-                <img src={image} alt="" className='h-68 w-96' />
+                <img src={image} alt="" className='h-68 w-96 rounded-tr-lg' />
             </div>
         </div>
     );
